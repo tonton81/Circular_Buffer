d9f95639d7878a8819f20964bcd2c2cd0d200b9e
Antonio Brewer <yodabox64@hotmail.com>
2018-09-29 15:19:50 -0400

    Add files via upload

---
+++ examples/CBA_Remove_Example/CBA_Remove_Example.ino
@@ -0,0 +1,22 @@
+#include "IFCT.h"
+
+Circular_Buffer<uint32_t, 8, 5> cba;
+
+void setup() {
+  Serial.begin(1);
+  delay(1000);
+  uint32_t buf1[5] = { 1, 2, 3, 4 , 5 };
+  uint32_t buf2[5] = {  6, 7, 8, 9, 10 };
+  uint32_t buf3[5] = { 11, 12, 13, 14, 15 };
+  cba.push_back(buf1, 5);
+  cba.push_back(buf2, 5);
+  cba.push_back(buf3, 5);
+  cba.list();
+  cba.remove(2);
+  cba.list();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
